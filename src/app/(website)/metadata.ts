583ccfe123394c5906a6585ae5758238dd6baaef
satyabrata20011 <sssatyabrata2001@gmail.com>
2025-06-18 23:24:07 +0530

    Update metadata.ts

--- src/app/(website)/metadata.ts
+++ src/app/(website)/metadata.ts
@@ -1,7 +1,7 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Slidely - AI Presentation Builder",
+  title: "Qubemonk - AI Presentation Builder",
   description:
     "Transform your ideas into stunning presentations in minutes with AI-powered slide creation.",
 };
